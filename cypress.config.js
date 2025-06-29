2dd2ced2cce84b080bded8de332f39bab56f130f
Leonardo Fernandez <leofernandez03@gmail.com>
2025-06-29 19:04:59 -0300

    yml reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  screenshotsFolder: 'images',
+  reporterOptions: {
+    videoOnFailOnly: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl : 'https://www.saucedemo.com/v1/',
   },
