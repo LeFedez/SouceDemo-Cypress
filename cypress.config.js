f70f5b5426071e85b52de21aac3c2f829e47697e
Leonardo Fernandez <leofernandez03@gmail.com>
2025-06-30 18:01:30 -0300

    nuevo reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  video: true,
-  screenshotsFolder: 'images',
-  reporterOptions: {
-    videoOnFailOnly: true
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
     },
-    baseUrl : 'https://www.saucedemo.com/v1/',
+    // Habilita el reporter
+    reporter: 'cypress-json-reporter',
+    reporterOptions: {
+      // El nombre del archivo de salida
+      jsonFile: 'cypress-results.json'
+    },
   },
 });
