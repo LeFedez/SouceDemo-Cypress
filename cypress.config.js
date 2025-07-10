f54c75f5899745f5f96bb665e22acfd05ddb793b
Leonardo Fernandez <leofernandez03@gmail.com>
2025-07-10 15:01:35 -0300

    probando lefedez 18

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,16 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.saucedemo.com/v1/', // Cambia esta URL por la de tu aplicación real
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Asegúrate de que coincide con tus tests
-    supportFile: 'cypress/support/e2e.js'
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: true,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    reportPageTitle: 'Reporte de Pruebas Cypress'
-  },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true
+    }
+  }
 });
-
